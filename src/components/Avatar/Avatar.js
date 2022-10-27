fdd80884848139160b10c72e3162d3642bbbc295
Thanh Vuong <68543789+ThanhVuong0904@users.noreply.github.com>
2022-10-28 00:58:41 +0700

    Update Avatar.js

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -1,6 +1,5 @@
 import classNames from "classnames/bind";
 import styles from "./Avatar.module.scss";
-import Jennie from "../../assets/images/jennie.jpg";
 
 const cx = classNames.bind(styles);
 const Avatar = ({ avatar, isStory, className }) => {
